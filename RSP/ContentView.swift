10fda0898857a74295dc5cbcfe34805437633801
AniketKumar090 <kumaraniket009@gmail.com>
2024-07-23 02:18:52 +0530

    Initial Commit

---
+++ RSP/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RSP
+//
+//  Created by Aniket Kumar on 23/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RSP/RSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSPApp.swift
+//  RSP
+//
+//  Created by Aniket Kumar on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
